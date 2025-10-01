9cd10936de2824fcc180b02634f1d4d1638270eb
Sakina <70714610+slougue1@users.noreply.github.com>
2025-10-01 14:48:57 -0400

    Create variables.tf
    variables.tf initial commit

---
+++ Terraform/Modules/ec2/variables.tf
@@ -0,0 +1,11 @@
+variable "ami_value" {
+   description = "The AMI ID to use for my ec2 instance"
+}
+
+variable "instance_type_value" {
+   description = "The instance type to use for my ec2 instance"
+}
+
+variable "subnet_id" {
+   description = "The subnet ID to use for my ec2 instance"
+}
